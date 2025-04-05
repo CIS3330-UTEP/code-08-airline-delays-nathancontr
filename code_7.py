f77726dc0952ff90fc4f8d8b0a031ca108f1f38c
nathancontr <ncontreras9@miners.utep.edu>
2025-04-05 10:42:26 -0600

    section 1

--- code_7.py
+++ code_7.py
@@ -1,12 +1,26 @@
 import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import seaborn as sns
 #If any of this libraries is missing from your computer. Please install them using pip.
 
 filename = 'Flight_Delays_2018.csv'
+df = pd.read_csv(filename)
 
 #ARR_DELAY is the column name that should be used as dependent variable (Y).
 
 
-#       first do a heatmap so you can see what variables affect ARR_DELAY
+#####   Section 1: Heatmap 
+
+corr_matrix = df.corr(numeric_only = True).round(2)
+
+sns.heatmap(corr_matrix, annot = True)
+
+plt.show()
+input()
+
+#####   Section 2: Regression
+
+
+
 
